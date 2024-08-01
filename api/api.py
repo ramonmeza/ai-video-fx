6a058da2e64ba162b1b7ae1764307f115ad4e855
Ramon Meza <ramon_meza@live.com>
2024-07-31 22:36:26 -0400

    Loading model and returning inference image

--- api/api.py
+++ api/api.py
@@ -1,8 +1,12 @@
-from asyncio import sleep
-
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from PIL import Image
+from transformers import pipeline
 
+
+# initialization
 app = FastAPI()
 
 origins = [
@@ -20,6 +24,15 @@ app.add_middleware(
 )
 
 
+# functions
+def image_to_bytes(image: Image):
+    image_bytes = BytesIO()
+    image.save(image_bytes, format="JPEG", quality=85)
+    image_bytes.seek(0)
+    yield from image_bytes
+
+
+# routes
 @app.get("/")
 async def get_info():
     return {"version": "0.0.1"}
@@ -29,13 +42,35 @@ async def get_info():
 async def get_models():
     return {
         "models": [
-            "prs-eth/marigold-depth-v1-0",
             "LiheYoung/depth-anything-small-hf",
+            # "prs-eth/marigold-depth-v1-0",
         ]
     }
 
 
 @app.post("/generate")
 async def post_generate(model: str, file: UploadFile):
-    await sleep(10)
-    return {"model": model, "filename": file.filename}
+    try:
+        # load video file
+        # for each frame (as a coroutine?)
+        #   apply model to frame
+        #
+        print("Reading file...", end="")
+        content = await file.read()
+        print("Done")
+
+        print("Creating pipeline...", end="")
+        pipe = pipeline(task="depth-estimation", model=model)
+        print("Done")
+
+        print("Loading image...", end="")
+        image = Image.open(BytesIO(content))
+        print("Done")
+
+        print("Generating depth...", end="")
+        result = pipe(image)["depth"]
+        print("Done")
+
+        return StreamingResponse(image_to_bytes(result), media_type="image/jpg")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
           <div>
             <label type="button" htmlFor="FileInput" className="inline px-3 py-1 text-white rounded-sm bg-sky-700 hover:bg-sky-400">Select Video</label>
             <span id="filename"></span>
-            <input type="file" id="FileInput" name="file" className="hidden" onChange={updateFilename}></input>
+            <input type="file" id="FileInput" name="file" className="hidden" onChange={updateFilename} accept="video/*"></input>
           </div>
 
           <div>
@@ -66,7 +66,8 @@ export default function Home() {
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="black" strokeWidth="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
-              </button> :
+              </button>
+              :
               <button type="button" onClick={submitForm} className="block px-3 py-1 text-white rounded-sm bg-sky-700 hover:bg-sky-400">Submit</button>
           }
         </form>
